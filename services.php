692818be969ffb3f7f8afe2365131d343bd29290
Vineet kumar <vineetkumar03@gmail.com>
2013-11-18 00:30:38 +0530

    third commi

---
+++ services.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+ <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"> 
+ <title abbr="" img src="img_flwr.jpg">
+E Web Design and Development
+</title>
+<link rel="icon" href="img_flwr.gif" type="image/x-icon">
+</head>
+<head>
+<style type="text/css">
+ul
+{
+float:left;
+width:100%;
+padding:2px;
+margin:2px;
+list-style-type:none;
+}
+a
+{
+float:left;
+width:5em;
+text-decoration:none;
+color:white;
+background-color:purple;
+padding:0.2em 0.6em;
+border-right:1px solid white;
+}
+a:hover {background-color:#ff3300;}
+li {display:inline;}
+.center
+{
+margin:auto;
+width:55%;
+background-color:#b0e0e6;
+}
+div
+{
+border:1px solid black;
+padding:10px;
+background-image:url('img_flwr.gif');
+background-repeat:no-repeat;
+background-position:right;
+text-shadow: 5px 5px 5px #FF0000
+}
+</style>
+</head>
+<body class="center">
+<div>
+<h1>E Web Design and Development <h1>
+</div>
+
+<?php
+include_once("header.php");
+?>
+</br>
+<img  src="250px.gif" width="100%" height="1" /></br>
+<h2 style="text-align:center;color:red;">Services</h2>
+<img src="250px.gif" width="100%" height="1" /></br>
+<p>
+<b>
+Business Solutions for Industry Leadership and Competitive Advantage</b></br></br>
+The changing business and technology landscape demands business and technological agility driving new business imperatives. The enterprise application and technology landscape has to constantly evolve to ensure that the business will continue to innovate for sustained success in the foreseeable future.</br></br>
+Business Application Services enables customers create successful and adaptive businesses through a robust business architecture, process transformation and innovation.</br></br>
+Our integrated business solutions across the application and technology landscape --- transform business processes, maximize and extend value of package applications, aggregate application capabilities to drive greater collaboration and commerce with customers, integrate and optimize application architecture aligning it to business priorities, enable secure IT operations enforcing consistent risk and compliance mandates and drive a total quality approach for applications and technology solutions.</br></br>
+We help drive business innovation through integrating next generation technology into the enterprise IT landscape for our customers. This includes Web Applications, Cloud Computing, Enterprise Mobility, Social Media and Digital Marketing that drives interactive experiences creating increased differentiation and revenues online.</br></br>
+<b>What we offer?</b></br><b>
+Enterprise Applications</b></br></br>
+Business Process transformation solutions across the value chain in the areas of: ERP, Supply Chain Management and Customer Relationship Management across leading packages including SAP and Oracle. We help global organizations implement & run Industry specific best practice processes maximizing and extending business value with reduced TCO and enhanced productivity.</br></br>
+<b>Business Collaboration and Customer Experience</b></br></br>
+B2C, B2B, B2E collaboration and commerce solutions driving business growth and workforce effectiveness for organizations. Our customer experience solutions enable organizations create a consistent multi-channel brand experience and drive customer engagement & loyalty.</br></br>
+<b>Enterprise Integration</b></br></br>
+Comprehensive portfolio of service-oriented integration solutions, including Application to Application integration and Business to Business integration, enable seamless cross-functional business processes within the enterprise and across the extended value chain.</br></br>
+<b>Enterprise Security Solutions</b></br></br>
+Enhance performance of information security and compliance programs to adapt with agility to constantly evolving and new business & IT risks.</br></br>
+<b>Testing Services</b></br></br>
+Evolve a comprehensive testing strategy with innovative solutions, approach and commercial models to drive a total quality assurance.</br></br>
+<b>Emerging Technologies</b></br></br>
+We enable organizations in navigating and adopting new paradigms such as Cloud / SaaS, Sustainability, Digital Marketing, and Social Computing delivering faster business results on investments to gain a competitive edge.</br></br>
+</p>
+
+
+<?php
+Include_once("footer.php");
+?>
+
+</body>
+<html>
